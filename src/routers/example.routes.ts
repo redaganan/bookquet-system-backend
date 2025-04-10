30638fff8a7e953ba9aebb20ce0b3c611031efc0
redaganan <weweako36@gmail.com>
2025-04-10 19:11:48 +0800

    chore: add comment for api routing in example.routes.ts

--- src/routers/example.routes.ts
+++ src/routers/example.routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 
 import * as exampleController from "../controllers/example.controller";
 
+// api routing
 export default async (fastify: FastifyInstance) => {
     fastify.get("/", exampleController.exampleController);
     fastify.post("/upload", exampleController.uploadController);
